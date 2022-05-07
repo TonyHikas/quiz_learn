4c0a7e15c26dca8494c8c775f9421c94d2ed6e77
Tony Hikas <a.class@inbox.ru>
2022-05-07 15:21:14 +0300

    Fix static

--- settings/settings.py
+++ settings/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/quiz_learn/static/'
+STATIC_ROOT = '/code/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
